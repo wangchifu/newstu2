08324e8a8a41d9737d717d66da4b98242e507d31
wangchifu <wangchifu@hdes.chc.edu.tw>
2024-12-27 10:27:59 +0800

    yt

--- resources/views/teach.blade.php
+++ resources/views/teach.blade.php
@@ -13,7 +13,13 @@
       <div class="card">
         <div class="card-body">
           <h5 class="card-title">教學影片</h5>
-
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/Al_QSjiLyFQ?si=k8JFW1hyVyI8Zp7y" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+          <hr>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/VVepXegIXUw?si=uyPbIY4c4TPOARmc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+          <hr>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/AxE6VyiHHcU?si=oFRZuZFS8pCkLy9k" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+          <hr>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/CvokT96_kKM?si=tWQDzgmSLmXtewzR" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </div>
       </div>
     </div>
