e4875d24fd4c20c32c83ac19b70ce86f179e2ed3
wangchifu <wangchifu@hdes.chc.edu.tw>
2025-02-24 10:49:35 +0800

    openid

--- app/Http/Controllers/OpenIDController.php
+++ app/Http/Controllers/OpenIDController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use App\Models\School;
 
 // RP申請獲得
 define('CLIENT_ID', env('CLIENT_ID'));
