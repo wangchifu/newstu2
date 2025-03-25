6fd504a5c81a0cfb79ef0fb0f924a2104b220bbc
wangchifu <wangchifu@hdes.chc.edu.tw>
2025-03-25 08:43:58 +0800

    fix

--- resources/views/teach.blade.php
+++ resources/views/teach.blade.php
@@ -13,12 +13,19 @@
       <div class="card">
         <div class="card-body">
           <h5 class="card-title">教學影片</h5>
+          <h6>1.各校從「校務系統」下載「學生名冊」。</h6>
           <iframe width="560" height="315" src="https://www.youtube.com/embed/Al_QSjiLyFQ?si=k8JFW1hyVyI8Zp7y" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
           <hr>
+          <h6>2-1.各校上傳「學生名冊」至系統，並設定學生的各種狀況。</h6>
           <iframe width="560" height="315" src="https://www.youtube.com/embed/VVepXegIXUw?si=uyPbIY4c4TPOARmc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
           <hr>
+          <h6>2-2.各校可自行練習編班，並在「編班中心」完成正式編班後，自行下載「編班檔案」供上傳至「校務系統」。</h6>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/6bThmTzARaA?si=zCGPA0kpCsKYq_8y" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+          <hr>
+          <h6>3.「編班中心」的操作。</h6>
           <iframe width="560" height="315" src="https://www.youtube.com/embed/AxE6VyiHHcU?si=oFRZuZFS8pCkLy9k" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
           <hr>
+          <h6>4.「編班中心」指定下一所學校。</h6>
           <iframe width="560" height="315" src="https://www.youtube.com/embed/CvokT96_kKM?si=tWQDzgmSLmXtewzR" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </div>
       </div>
